ea51fa5fd1e67d548e45e97beead787640cc3b16
Lida <163001+Lida@users.noreply.github.com>
2020-03-25 01:20:56 -0400

    Added physics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React, { useEffect } from "react";
 import "./styles.css";
 import "aframe";
+import "aframe-physics-system";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-AFRAME.registerComponent("hello-world", {
-  init: function() {
-    console.log("Hello, World!");
-  }
-});
 
 const jquery = document.createElement("script");
 jquery.src = "https://code.jquery.com/jquery-3.4.1.min.js";
@@ -256,26 +251,44 @@ function changeAudioOutput(selected) {
 }
 
 export default function App() {
+  // let boardName =
+  //   window.Game.BasicModule.Map[4].BoardPicker.Board._attributes.image;
+  let boardName = encodeURIComponent("Board OTB.jpg");
+  //https://raw.githubusercontent.com/Lida/GameAndChat/master/public/Pandemic/images/Board%20OTB.jpg
   useEffect(() => {
     const content = `
-    <a-scene embedded>
-    <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" />
-    <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" />
+    <a-scene embedded physics="driver: ammo; debug: false; debugDrawMode: 1;">
+      <a-assets>
+        <img id="board" src="https://raw.githubusercontent.com/Lida/GameAndChat/master/public/Pandemic/images/${boardName}" />
+      </a-assets>
+    <a-entity id="rig" position="0 0 0" rotation="-65 0 0">
+      <a-camera fov="50"></a-camera>
+    </a-entity>
+    <a-image
+      src= "#board"
+      ammo-body="type: static"
+      ammo-shape="type: box"
+      position="0 -2 -3"
+      rotation="-90 0 0"
+      width="6"
+      height="4">
+      </a-image>
+    <a-box ammo-body="type: dynamic" ammo-shape="type: box" position="-1 0.5 -3" rotation="44 44 0" color="#4CC3D9">
+    </a-box>
+    <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E">
+    </a-sphere>
     <a-cylinder
       position="1 0.75 -3"
       radius="0.5"
       height="1.5"
       color="#FFC65D"
-    />
-    <a-plane
-      position="0 0 -4"
-      rotation="-90 0 0"
-      width="4"
-      height="4"
-      color="#7BC8A4"
-    />
-    <a-entity hello-world /> 
-    <a-sky color="#ECECEC" />
+    >
+    </a-cylinder>
+    <a-light type="ambient" color="#445451"></a-light>
+      
+    <a-light type="point" intensity="2" position="2 4 4"></a-light>
+
+    <a-sky color="#333333" />
   </a-scene>
     `;
     document.getElementById("aframe").innerHTML = content;
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,22 @@ import App from "./App";
 
 import convert from "xml-js";
 
-fetch("Pandemic/buildFile").then(response => {
-  response.text().then(text => {
-    window.Game = convert.xml2js(text, {
-      compact: true,
-      elementNameFn: function(val) {
-        return val.split(".").pop();
-      }
-    });
-  });
-});
+// const fn = async ({ signal }) => {
+//   await fetch("Pandemic/buildFile");
+// };
+
+// fn();
+
+// .then(response => {
+//   response.text().then(text => {
+//     window.Game = convert.xml2js(text, {
+//       compact: true,
+//       elementNameFn: function(val) {
+//         return val.split(".").pop();
+//       }
+//     });
+//   });
+// });
 // fetch("./buildFile").then(response => {
 //   window.GameFile = convert.xml2js(response.body, { compact: true });
 // });
